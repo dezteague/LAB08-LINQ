d48b93ae52c48dc6e9beacfe813d113453e17d6e
Deziree' Teague <dez.teague@gmail.com>
2019-01-17 23:32:34 -0800

    outlined logic for 1-3

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -20,3 +20,20 @@ namespace LINQ
         }
     }
 }
+
+//LINQ LOGIC
+//    1. Output all of the neighborhoods in this data list
+//      foreach(Properties n in result)
+
+//    2. Filter out all the neighborhoods that do not have any names
+//      result = from n in list
+//      where n.neighborhood.Length != 0
+//      select n;
+
+//    3. Remove the Duplicates
+//       result = from n in list
+//       where n.Duplicate == false
+//       select n;
+
+//    4. Rewrite the queries from above, and consolidate all into one single query.
+//    5. Rewrite at least one of these questions only using the opposing method(example: Use LINQ instead of a Lambda and vice versa.)
